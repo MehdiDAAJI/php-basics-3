5723b57ec5f98ce46562bc693e2ef71935704ec2
MehdiDAAJI <mehdidaaji@gmail.com>
2022-10-25 16:16:26 +0200

    first commit

--- public/index.php
+++ public/index.php
@@ -1,8 +1,8 @@
 <?php
-require"../src/function.php";
+require "../src/function.php";
 
 echo sayHello();
-echo"<br><br>";
+echo "<br><br>";
 echo whoAmI("Ozymandias");
-echo"<br><br>";
-echo callByFullName(lastName : "Beugnet", name : "Benjamin");
+echo "<br><br>";
+echo callByFullName(lastName: "Daaji", name: "Mehdi");
